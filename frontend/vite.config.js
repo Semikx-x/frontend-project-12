b8550e96497d4efd4fc3baf1a48dafbcb21ae552
Semikx-x <semen.samsonov@mail.ru>
2025-08-27 18:53:19 +0300

    1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,11 @@ export default defineConfig({
   server: {
     port: 5002,
     proxy: {
+      // Проксируем запросы к API
       '/api': {
         target: 'http://localhost:5001',
       },
+      // Проксируем WebSocket соединения
       '/socket.io': {
         target: 'ws://localhost:5001',
         ws: true,
