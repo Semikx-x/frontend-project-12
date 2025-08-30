f607df1e415d231dedb7153870b4f8d1fc3ad9bb
Semikx-x <semen.samsonov@mail.ru>
2025-08-30 15:25:15 +0300

    добавл подстановочный путь

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import {Login} from './pages/login.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+      <Route path="login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
