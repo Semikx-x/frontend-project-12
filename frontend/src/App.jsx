9011479650a9110b65df46fefe81e857a9e61e6b
Semikx-x <semen.samsonov@mail.ru>
2025-09-01 16:00:07 +0300

    1`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {<Login/>}
         <Route path="/login" element={<Login/>}/>
         <Route path="/*" element={<NotF/>}/>
       </Routes>
