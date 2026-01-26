48e0a5193dc1d7b7e9962da3b9696f772cf31a77
Semikx-x <semen.samsonov@mail.ru>
2026-01-26 11:23:51 +0300

    fix

--- frontend/src/components/pages/login.jsx
+++ frontend/src/components/pages/login.jsx
@@ -2,19 +2,23 @@ import { LoginForm } from "../Form/Form.jsx"
 
 const Login = () => {
   return (
-    <div className="col-12 col-md-8 col-xxl-6">
-      <div className="card shadow-sm">
-        <div className="card-body row p-5">
-          <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-          </div>
-          <LoginForm/>
-        </div>
-          <div className="card-footer p-4">
-            <div className="text-center">
-              <span>Нет аккаунта?</span>
-              <a href="/signup">Регистрация</a>
+    <div className="container-fluid vh-100 d-flex justify-content-center align-items-center">
+      <div className="row">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm">
+            <div className="card-body row p-5">
+              <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                <LoginForm/>
+              </div>
             </div>
+              <div className="card-footer p-4">
+                <div className="text-center">
+                  <span>Нет аккаунта?</span>
+                  <a href="/signup">Регистрация</a>
+                </div>
+              </div>
           </div>
+        </div>
       </div>
     </div>
   )
