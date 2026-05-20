8e77fc7fc6a54c2535ef02962e5f48be02de074c
Semikx-x <semen.samsonov@mail.ru>
2026-05-20 18:11:13 +0300

    fix

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { ToastContainer, Bounce } from 'react-toastify';
+import { useTranslation } from 'react-i18next'
+
 
 export const Layout = () => {
  
+  const { t } = useTranslation()
+
   return (
     <div>
+      <header>
+        <a href="/">{t('menu.chat')}</a>
+      </header>
       <ToastContainer
         position="bottom-right"
         autoClose={5000}
